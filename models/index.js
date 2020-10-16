7cf1fde56855ad8ea76bb27c66273b8422728eab
hansonjw <hansonjw@gmail.com>
2020-10-16 18:16:58 -0500

    Add Product routes

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 Product.belongsTo(Category, {
-  foreignKey: 'id'
+  foreignKey: 'cateogory_id'
 });
 
 Category.hasMany(Product, {
